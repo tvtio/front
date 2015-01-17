fa9488226dbab36a74120338a0715e8dfb5ec435
Raül Pérez <repejota@gmail.com>
2015-01-17 05:01:20 +0100

    front at port 80

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ app.get('/', function (req, res) {
     res.send('Hello World!')
 })
 
-var server = app.listen(3000, function () {
+var server = app.listen(80, function () {
     var host = server.address().address
     var port = server.address().port
-    console.log('Example app listening at http://%s:%s', host, port)
+    console.log('moview.io front listening at http://%s:%s', host, port)
 })
 
