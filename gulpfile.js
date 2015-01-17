26ca752e398594366d06f988c1bafff855c6b288
Raül Pérez <repejota@gmail.com>
2015-01-17 05:38:12 +0100

    added jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,10 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+
+gulp.task('jshint', function () {
+    gulp.src('*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
 
 gulp.task('default', [], function() {});
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
 
 var server = app.listen(80, function () {
-    var host = server.address().address
-    var port = server.address().port
-    console.log('moview.io front listening at http://%s:%s', host, port)
-})
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('moview.io front listening at http://%s:%s', host, port);
+});
