ce28ec0450a5e64703c444a80787adea56245928
Raül Pérez <repejota@gmail.com>
2015-01-17 21:04:16 +0100

    auth twitter

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 var express = require('express');
+var session = require('express-session');
+var logger = require('express-logger');
+
+var routes = require('./lib/routes');
+
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+app.use(logger({ path: "./log/app.log" }));
 
-var server = app.listen(80, function () {
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/src/views');
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.get('/', routes.index);
+app.get('/home', routes.home);
+
+app.get('/logout', routes.authLogout);
+app.get('/auth/twitter', routes.authTwitter);
+app.get('/auth/twitter/callback', routes.authTwitterCallback);
+
+var server = app.listen(process.env.PORT || 8081, function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('moview.io front listening at http://%s:%s', host, port);
