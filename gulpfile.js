6661027683bc1956b837521ae192278a0bcc60f7
Raül Pérez <repejota@gmail.com>
2015-01-18 14:40:26 +0100

    Added twitter login

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var less = require('gulp-less');
 
 gulp.task('jshint', function () {
     gulp.src('*.js')
@@ -7,4 +8,12 @@ gulp.task('jshint', function () {
         .pipe(jshint.reporter('default'));
 });
 
-gulp.task('default', [], function() {});
+gulp.task('less', function () {
+    gulp.src('./src/less/**/*.less')
+        .pipe(less({
+            paths: [ ]
+        }))
+        .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('default', ['jshint', 'less'], function() {});
--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var session = require('express-session');
 var logger = require('express-logger');
+var favicon = require('serve-favicon');
+var servestatic = require('serve-static')
 
 var routes = require('./lib/routes');
 
 var app = express();
 
 app.use(logger({ path: "./log/app.log" }));
+app.use(favicon(__dirname + '/public/img/favicon.png'));
+app.use(servestatic(__dirname + '/public'));
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/src/views');
