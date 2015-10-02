3c28c9c5d05d6efa3761f1a8a25e9ffe5c32b700
Raül Pérez <repejota@gmail.com>
2015-10-02 23:56:50 +0200

    Added middleware log, gzip and updated about template

--- server.go
+++ server.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"compress/gzip"
 	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/carbocation/interpose"
+	"github.com/carbocation/interpose/middleware"
 	"github.com/julienschmidt/httprouter"
 	"github.com/tvtio/front/routes"
 )
@@ -13,6 +16,13 @@ func main() {
 	host := "localhost"
 	port := "8080"
 	serverAddress := host + ":" + port
+
+	middle := interpose.New()
+	// Using Gorilla framework's combined logger
+	middle.Use(middleware.GorillaLog())
+	//Using Negroni's Gzip functionality
+	middle.Use(middleware.NegroniGzip(gzip.DefaultCompression))
+
 	router := httprouter.New()
 	router.GET("/", routes.Index)
 	router.GET("/search", routes.Search)
@@ -23,6 +33,9 @@ func main() {
 	router.ServeFiles("/css/*filepath", http.Dir("./templates/css"))
 	router.ServeFiles("/js/*filepath", http.Dir("./templates/js"))
 	router.ServeFiles("/img/*filepath", http.Dir("./templates/img"))
+
+	middle.UseHandler(router)
+
 	fmt.Println("Listening at http://" + serverAddress)
-	log.Fatal(http.ListenAndServe(serverAddress, router))
+	log.Fatal(http.ListenAndServe(serverAddress, middle))
 }
