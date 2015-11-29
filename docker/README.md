1e6c121a88db9209717fce158790b3d2d080bba2
Raül Pérez <repejota@gmail.com>
2015-11-29 16:50:53 +0100

    Dockerized tvt.io

--- docker/README.md
+++ docker/README.md
@@ -11,3 +11,7 @@ $ docker build -t tvtio -f docker/Dockerfile .
 ```
 $ docker run -ti --rm tvtio .
 ```
+
+Exports containerized service from port 8080 to port 80 on the host machine.
+
+Try: http://<docker-host>/
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 set -e
-
-alias tmux="tmux -2"
-
-SESSION="tvtio"
-
-tmux new-session -d -s $SESSION
-tmux rename-window "vim"
-tmux send-keys "nvim" C-m
-tmux new-window -t $SESSION:1 -n "shell"
-tmux select-window -t $SESSION:0
-tmux attach-session -t $SESSION
+make install
+front
